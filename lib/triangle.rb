9db95d6bee4997d8d25d383f57746437928c7ec1
Jelena <jelena.durovic@powerhrg.com>
2021-08-11 15:58:44 -0400

    commit tree 2efe98509d4b4af869c7c30816f4e2523f28e140

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,54 @@
+require "pry"
+
+
 class Triangle
-  # write code here
+
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    
+  end 
+
+
+
+  def inspect_triangle_validation
+  
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
+      raise  TriangleError
+
+    elsif @side_one > @side_two + @side_three || @side_two >= @side_one + @side_three || @side_three > @side_one + @side_two
+      raise TriangleError
+  
+    end 
+  
+
+  end  
+
+
+
+  def kind 
+    inspect_triangle_validation
+    if side_one == side_two && side_one == side_three
+        :equilateral
+    elsif side_one == side_two || side_one == side_three || side_two == side_three
+        :isosceles
+    else
+      :scalene
+    end 
+  end 
+
+  
+
+
+
+      class TriangleError < StandardError
+
+      end 
+      
+
+  
 end
